b4b4fd8cb6fbe997c085e2451782dfec55641ee0
Nitin2004577 <shahnitin500@gmail.com>
2024-07-11 13:44:53 +0545

    Loading

---
+++ WanderMate2.0-master/src/helper/data.js
@@ -0,0 +1,51 @@
+import { LandinHeaderLinks } from './data'
+export const landingFooterLinks = [
+  {
+    id: 1,
+    title: "Facebook",
+    link: "https://www.facebook.com",
+  },
+  {
+    id: 2,
+    title: "X",
+    link: "https://www.x.com",
+  },
+  {
+    id: 3,
+    title: "Instagram",
+    link: "https://www.instagram.com",
+  },
+  {
+    id: 4,
+    title: "Youtube",
+    link: "https://www.youtube.com",
+  },
+  {
+    id: 5,
+    title: "Google",
+    link: "https://www.google.com",
+  },
+];
+
+const LandinHeaderLinks = [
+  {
+    id: 1,
+    title: "About",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Contact",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "sign-up",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "sale",
+    link: "#",
+  },
+]
